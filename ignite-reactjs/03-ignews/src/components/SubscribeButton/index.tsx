e2f2152806f9156cf9d59c62ca4658502a129aef
Matheus Limão Brites <majortheus@gmail.com>
2021-05-22 13:12:48 -0300

    finished next module

---
+++ ignite-reactjs/03-ignews/src/components/SubscribeButton/index.tsx
@@ -0,0 +1,13 @@
+import styles from 'styles.module.sass'
+
+interface SubscribeButtonProps {
+  priceId: string;
+}
+
+export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  return (
+    <button type="button" className={styles.subscribeButton}>
+      Subscribe now
+    </button>
+  )
+}
