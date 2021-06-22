4c791376e258401618bdc52194de9fed6a2c35b2
Matheus Limão Brites <majortheus@gmail.com>
2021-06-22 13:21:54 -0300

    half way into Liftoff

---
+++ nlw06/reactnative-gameplay/App.tsx
@@ -0,0 +1,12 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { SignIn } from './src/screen/SignIn';
+
+export default function App() {
+  return (
+    <>
+      <StatusBar style="auto" />
+      <SignIn />
+    </>
+  );
+}
